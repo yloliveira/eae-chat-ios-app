71af56c18d7117e38d11a20897b351e5395020ef
Yan Oliveira <oliveira.yan02@gmail.com>
2022-10-03 15:34:16 -0300

    feat: add welcome screen's title animation

--- Eae Chat/Controllers/WelcomeViewController.swift
+++ Eae Chat/Controllers/WelcomeViewController.swift
@@ -9,14 +9,25 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        animateTitle()
     }
     
-
+    
+    private func animateTitle() {
+        let title = "💬 Eae"
+        titleLabel.text = ""
+        var index = 0.0
+        
+        for letter in title {
+            Timer.scheduledTimer(withTimeInterval: 0.2 * index, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            index += 1
+        }
+    }
 }
